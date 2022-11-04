7371d87a15dde5115db6ae142ec2226ae2b1e1d8
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-04 14:36:53 +0500

    if else statement

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    // PHP Conditional Statements
+    // Conditional Statements are used to perform actions based on different conditions. Sometimes when we write a program, we want to perform some different actions for different actions. We can solve this by using conditional statements. 
+
+    // In PHP we have these conditional statements:
+
+    // if Statement.
+    // if-else Statement
+    // If-elseif-else Statement
+    // Switch statement
+
+
+
+    ?>
+
+
+</body>
+
+</html>
